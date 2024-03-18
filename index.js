62234cd0c50cb4cd8dd4ba5bb6f36ab351495c07
kwame Adams <kwameadams77@gmail.com>
2024-03-18 12:42:50 +0000

    news

---
+++ index.js
@@ -0,0 +1,109 @@
+const apikey = 'a4c657cef384431c9316c191f0deb71f'
+
+const blogContainer = document.getElementById('blog-container');
+
+const searchField = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click',async ()=>{
+    const query = searchField.value.trim()
+    if(query !== ''){
+        try{
+        const articles = await fetchNewsQuery(query)
+        displayBlogs(articles)
+        
+
+        }catch(error){
+console.log('Error fetching news by query',error)
+        }
+
+    }
+})
+
+async function fetchNewsQuery(query){
+    try {
+        const apiUrl = `
+        https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apikey}`
+        
+        const res= await fetch(apiUrl);
+        const data = await res.json();
+        return data.articles
+        console.log(data)
+    
+            
+        } catch (error) {
+            console.error('error fetching random news',error)
+            return []
+        }
+}
+
+
+
+
+async function fetchRandomNews(){
+
+    try {
+    const apiUrl = `
+    https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apikey}`
+    
+    const res= await fetch(apiUrl);
+    const data = await res.json();
+    return data.articles
+    console.log(data)
+
+        
+    } catch (error) {
+        console.error('error fetching random news',error)
+        return []
+    }
+}
+
+
+function displayBlogs(articles){
+    blogContainer.innerHTML = ''
+    articles.forEach((article) =>{
+        const blogCard = document.createElement('div')
+        blogCard.classList.add('blog-card')
+        const img = document.createElement('img')
+        img.src = article.urlToImage
+        img.alt = article.title
+        const title = document.createElement('h2')
+       const truncatedTitle = article.title.length > 30 ? article.title.slice(0,30) + "....": article.title;
+       title.textContent = truncatedTitle;
+        const description = document.createElement('p')
+        const truncatedDes = article.description.length > 120 ? article.description.slice(0,120) + "....": article.description;
+        title.textContent = truncatedTitle;
+        description.textContent = truncatedDes;
+
+
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+
+        blogCard.addEventListener('click',()=>{
+            window.open(article.url,'_blank');
+        })
+
+        blogContainer.appendChild(blogCard)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+(async ()=>{
+    try {
+    const articles =    await fetchRandomNews()
+    displayBlogs(articles);
+    console.log(articles)
+    } catch (error) {
+        console.error('error fetching random news',error)
+    }
+})();
